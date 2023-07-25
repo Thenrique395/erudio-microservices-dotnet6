f501f04f5e32b530fd8d40ea1254a139ed7dbe9d
Henrique Santos <thenrique395@gmail.com>
2023-07-25 16:00:09 -0300

    my primary project

---
+++ GeekShooping/GeekShopping.ProductApi/Model/Context/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductApi.Model.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public MyContext(DbContextOptions<MyContext> dbContextOptions) : base(dbContextOptions)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
